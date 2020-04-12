25e64097d4732e977b9dd501f6e65dcafef890c5
Alexandrew <andrew.alexandrow97@gmail.com>
2020-04-12 13:14:00 +0300

    Refactoring code

--- src/js/pages/header.js
+++ src/js/pages/header.js
@@ -1,24 +1,24 @@
 import $ from 'jquery';
 
 export default $(() => {
-	$("#hamburgerMenu").on('click', function() {
-		$(this).toggleClass('is-active');
-		$('.nav').toggleClass('nav--active');
-		$('body').toggleClass('overflow-hidden');
-	})
-	
-	$('#nav').on('click', function(e){
-		if ( e.target.classList.contains('menu-item__link')) {
-			$('.menu-item__link--active').removeClass('menu-item__link--active');
-			$(e.target).addClass('menu-item__link--active');
-			
-			const currentId = $(e.target).attr('href');
-			$('html,body').stop().animate({ scrollTop: $(currentId).offset().top }, 1000);
-			e.preventDefault();
-			
-			$("#hamburgerMenu").removeClass('is-active');
-			$('.nav').removeClass('nav--active');
-			$('body').removeClass('overflow-hidden');
-		}
-	});
+  $("#hamburgerMenu").on('click', function () {
+    $(this).toggleClass('is-active');
+    $('.nav').toggleClass('nav--active');
+    $('body').toggleClass('overflow-hidden');
+  });
+
+  $('#nav').on('click', function (e) {
+    if (e.target.classList.contains('menu-item__link')) {
+      $('.menu-item__link--active').removeClass('menu-item__link--active');
+      $(e.target).addClass('menu-item__link--active');
+
+      const currentId = $(e.target).attr('href');
+      $('html,body').stop().animate({scrollTop: $(currentId).offset().top}, 1000);
+      e.preventDefault();
+
+      $("#hamburgerMenu").removeClass('is-active');
+      $('.nav').removeClass('nav--active');
+      $('body').removeClass('overflow-hidden');
+    }
+  });
 })
